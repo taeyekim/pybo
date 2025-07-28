47e99f70fc0d5cb948d06f6f03b7854c3dcad9ba
taeyekim <kam_ta_yon@naver.com>
2025-07-28 13:45:33 +0900

    404 화면 꾸미기

--- common/views.py
+++ common/views.py
@@ -21,3 +21,5 @@ def signup(request):
         form = UserForm()
     return render(request, 'common/signup.html', {'form': form})
 
+def page_not_found(request, exception):
+    return render(request, 'common/404.html', {})
