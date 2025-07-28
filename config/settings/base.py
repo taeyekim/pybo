c87cb0f97aeca9ab6eb002ebd0f8a3443923b6be
taeyekim <kam_ta_yon@naver.com>
2025-07-28 14:43:37 +0900

    로깅 설정 다시 함

--- config/settings/base.py
+++ config/settings/base.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # 로그인 성공후 이동하는 URL
 LOGIN_REDIRECT_URL = '/'
 
-DEFAULT_LOGGING = {
+LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'filters': {
